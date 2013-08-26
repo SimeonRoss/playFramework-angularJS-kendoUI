d453e63a978add6861d785d92e537caa473190fa
simmothefirst <simon.taran@gmail.com>
2013-08-27 09:54:41 +1000

    Added IBU calculator to factories in angular

--- public/javascripts/factories.js
+++ public/javascripts/factories.js
@@ -20,6 +20,40 @@ angular.module('BrewingTools.factories', ['ngResource'])
 
     return abvCalculator;
   }])
+  
   .factory('IbuQueryCalc', ['$resource', function($resource) {
     return $resource('/rest/ibu/calc');
-  }]);
+  }])
+
+  .factory('IbuCalculator', [ function() {
+    var ibuCalculator = {};
+
+    ibuCalculator.calculate = function(sg, additionTime, aaRating, qty, volume) {
+      var bigFactor = getBigFactor( sg );
+      var boilTimeFactor = getBoilTimeFactor( additionTime );
+      var decimalAAUtil = bigFactor * boilTimeFactor;
+      var mgPerL = getAlphaAcidConcentration( aaRating / 100, qty, volume );
+
+      var ibu = decimalAAUtil * mgPerL;
+
+      return round( ibu ).toDecimalPlaces( 2 );
+    };
+
+    ibuCalculator.getAlphaAcidConcentration = function( aaRating, grams, volume )
+    {
+      return (aaRating * grams * 1000) / volume;  
+    }
+
+    ibuCalculator.getBoilTimeFactor = function ( boilTime )
+    {
+      return (1 - Math.exp( -0.04 * boilTime )) / 4.15;
+    }
+
+    ibuCalculator.getBigFactor = function( gravity )
+    {
+      return 1.65 * Math.pow( 0.000125, gravity - 1 );
+    }
+
+    return ibuCalculator;
+  }]);;
+
